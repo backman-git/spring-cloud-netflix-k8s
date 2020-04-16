c610d0d96bc0b89d0ad09cc19d5abe9c8180ae37
root <root@backmanBook.localdomain>
2020-04-17 08:05:22 +0900

    first init

---
+++ echo/src/test/java/com/echoservice/echo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echoservice.echo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
