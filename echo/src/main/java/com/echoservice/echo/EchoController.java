99cbb8ab9e006d9ba843eb744e05d9394fac9bdd
yuzhenxie <yujhen5@gmail.com>
2020-04-19 20:11:31 +0900

    indent

--- echo/src/main/java/com/echoservice/echo/EchoController.java
+++ echo/src/main/java/com/echoservice/echo/EchoController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EchoController{
 
-  private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, %s!";
+	  private final AtomicLong counter = new AtomicLong();
+    
     @GetMapping("echo")
     public Greeting greeting(@RequestParam(value = "name",defaultValue = "world") String name){
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
