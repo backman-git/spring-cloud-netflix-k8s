da198336cc667618ca0a778e5241a52267656e5c
yuzhenxie <yujhen5@gmail.com>
2020-04-20 18:29:51 +0900

    add config server and configuration folder

---
+++ config-server/src/test/java/com/springcloudnetflixk8s/config/ConfigServerTests.java
@@ -0,0 +1,13 @@
+package com.springcloudnetflixk8s.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CConfigServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
